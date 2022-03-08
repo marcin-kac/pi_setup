01bec16b584632649f44eeef8fdfb255933b0a08
Marcin <marcin@apples-MacBook-Pro.local>
2022-03-08 17:10:30 +0000

    php

--- upload.php
+++ upload.php
@@ -8,9 +8,9 @@ $location = "cfg/".$filename;
 
 /* Permanently save the file upload to the upload folder */
 if ( move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $location) ) {
-  echo '<p>The HTML5 and php file upload was a success!</p>';
+  echo '<p>file upload was a success!</p>';
 } else {
-  echo '<p>The php and HTML5 file upload failed.</p>';
+  echo '<p>file upload failed.</p>';
 }
 
 ?>
